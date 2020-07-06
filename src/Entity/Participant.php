99bcd2bb687cd762f9e4302bc0f4f4bfc87376d6
edouard.peyrot <edouard.peyrot1@gmail.com>
2020-07-06 16:45:01 +0200

    Use assert

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
-
+use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
  */
