f055ff18d31fca64569354c7db48186ab4edc4bf
Etienne <etienne.fdarcier@hotmail.fr>
2020-07-06 17:12:51 +0200

    Login/logout: Remember me OK taper /login pour se connecter taper /logout pour se déconnecter directement

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,13 +10,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
+     * Method to login with user credentials (email+password)
      * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response Return an error or login the user
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('accueil');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -31,6 +34,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        //TODO write a method here...
     }
 }
