dcaeacd62df00fcb6655cb0f05bfb2d796f5217f
Etienne <etienne.fdarcier@hotmail.fr>
2020-07-07 12:37:54 +0200

    Ajouter nouveaux campus OK

--- src/Form/CampusType.php
+++ src/Form/CampusType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,7 @@ class CampusType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextType::class)
         ;
     }
 
