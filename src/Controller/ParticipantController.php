603d1a4855eb0731981fd59dadba6725ef217852
matthieuS <matthieu.sourbier@outlook.com>
2020-07-08 16:52:30 +0200

    version matthieu 08.07.20 edit fonctionnel new fonctionnel

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Route("/participant")
+ */
+class ParticipantController extends AbstractController
+{
+    private $passwordEncoder;
+    public  function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this -> passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/", name="participant_index", methods={"GET"})
+     */
+    public function index(ParticipantRepository $participantRepository): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'participants' => $participantRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="participant_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $participant = new Participant();
+        $participant -> setPassword('123456');
+        $participant -> setToken('azerty');
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $passwordEncoded = $this->passwordEncoder->encodePassword($participant, '123456');
+            $participant->setPassword($passwordEncoded);
+            $participant->setToken(substr(str_replace('/', '',$passwordEncoded),50));
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('participant_index');
+        }
+
+        return $this->render('participant/new.html.twig', [
+            'participant' => $participant,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="participant_show", methods={"GET"})
+     */
+    public function show(Participant $participant): Response
+    {
+        return $this->render('participant/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="participant_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Participant $participant): Response
+    {
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('participant_index');
+        }
+
+        return $this->render('participant/edit.html.twig', [
+            'participant' => $participant,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="participant_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Participant $participant): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$participant->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($participant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('participant_index');
+    }
+}
---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles', CollectionType::class, [
+                'entry_type' => ChoiceType::class,
+                'entry_options' => [
+                    'label' => false,
+                    'choices' => [
+                        'ADMIN' => 'ROLE_ADMIN',
+                        'USER' => 'ROLE_USER'
+                    ],
+                ]
+            ])
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('pseudo')
+            ->add('campus')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
