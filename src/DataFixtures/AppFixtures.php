a8140c770a9b20409884c2e14276e18615a50e7e
Etienne <etienne.fdarcier@hotmail.fr>
2020-07-09 10:22:10 +0200

    Fixtures : added random ImageURL & actif = true

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,7 +12,6 @@ use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
