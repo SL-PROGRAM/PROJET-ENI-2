f632f7834581740d3ae7d557984be98b7842865b
Etienne <etienne.fdarcier@hotmail.fr>
2020-07-22 13:54:19 +0200

    Added comments on SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Participant;
-use App\Form\ParticipantType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
